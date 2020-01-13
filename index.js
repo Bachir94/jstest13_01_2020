e3928f12bfe6e9932d26a94a0c1992043a65c933
Nom <Email>
2020-01-13 14:18:31 +0100

    testaremettre

---
+++ index.js
@@ -0,0 +1,76 @@
+//Exo01
+
+let maBoolean = new Boolean("False") ;
+console.log(maBoolean);
+
+
+
+
+//Exo02
+
+let monArray = ['chat',9,'chien',126];
+console.log(monArray);
+
+
+//Exo03
+
+let monObjet = {
+    nom : 'bachir',
+    age : 25,
+    ville : 'Bruxelles',
+}
+console.log(monObjet);
+
+
+//Exo04
+
+let monString = 'salutation !';
+console.log(monString);
+
+
+//exo05
+
+let monInt = 45 ;
+console.log(monInt);
+
+
+//exo06
+
+let exo06 = {
+    prenom : '',
+    ville : '' ,
+  
+    quietudouvienstu(prenom, ville){
+        this.prenom = prenom 
+        this.ville = ville
+        
+
+    }
+};
+exo06.quietudouvienstu('Bachir','Molenbeek');
+
+console.log(exo06.quietudouvienstu);
+
+
+
+//exo07LAfunction
+
+//exo08Lacondition
+
+//exo09Laboucle
+let leNombre = 0;
+console.log(leNombre);
+
+while (leNombre < 9) {
+    leNombre++;
+}
+console.log(leNombre);
+
+//exo10laDAte
+let laDAte : {
+
+}
+//exo11StringtoString
+
+//exo15
+Math.random()
